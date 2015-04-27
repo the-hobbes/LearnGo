46f8bdf53bac6483a2d16a6aab048cd718707571
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-04-27 08:21:25 -0700

    Initial commit, a sqrt implementation

---
+++ sqrt.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "fmt"
+)
+
+func Sqrt(x float64) float64 {
+  z := 1.0
+  for i := 0; i < 10; i += 1 {
+    z -= ((z * z) - x) / (2 * z) // pretty close
+  }
+  return z
+}
+
+func main() {
+  fmt.Println(Sqrt(2))
+}
+
