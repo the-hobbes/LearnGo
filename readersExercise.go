816e7cfb2fbbbb2b223e3a5aed925f662e90b148
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-05-04 22:12:25 -0700

    added a readers interface exercise

---
+++ readersExercise.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "golang.org/x/tour/reader"
+)
+
+type MyReader struct{}
+
+func (mr MyReader) Read(b []byte) (i int, e error) {
+  for x := range b {
+    b[x] = 'A'
+  }
+  return len(b), nil
+}
+
+func main() {
+  reader.Validate(MyReader{})
+}
+
