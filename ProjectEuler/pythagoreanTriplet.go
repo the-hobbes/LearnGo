5161c7e8a551687371b5c4d6cd5c6a82a442c84a
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-06-23 08:55:50 -0700

    working on solution to pythagorean triplets

--- ProjectEuler/pythagoreanTriplet.go
+++ ProjectEuler/pythagoreanTriplet.go
@@ -5,11 +5,40 @@ import (
 	"os"
 )
 
+func sumSlice(input []int) int {
+	total := 0
+	for _, value := range input {
+		total += value
+	}
+	return total
+}
+
+func subsetSum(candidates []int, target int, partial []int) []int {
+	s := sumSlice(partial)
+	if s == target {
+		fmt.Println("Target found, numbers are:")
+		fmt.Println(partial)
+	}
+	if s >= target {
+		return
+	}
+	for i := 0; i < len(candidates); i++ {
+		n := candidates[i]
+		remaining := numbers[i+1:] // Golang slicing?
+		subsetSum(remaining, target, partial + [n]) // append instead of + [n]
+	}
+
+	return []int{-1, -1, -1}
+}
 
 func findTriplet(tripletSum int) ([]int) {
 	// given a number, find the pythagorean triplet the sum of which equals 
-	// that number. 
-	return []int{-1, -1, -1}
+	// that number. note that a < b < c.
+	var partial []int
+	candidates := []int{1,2,3,4,5,6,7,8,9,10}
+	result := subsetSum(candidates, tripletSum, partial)
+	
+	return result
 }
 
 func test() {
