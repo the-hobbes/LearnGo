1a424a4fd2b106d9f5b1c989b028069a867d0adc
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-06-27 18:46:39 -0700

    Theoretically, this is a solution to the pythagorean triplet problem. in practice however, it is far too inefficient and will only work for small numbers.

--- ProjectEuler/pythagoreanTriplet.go
+++ ProjectEuler/pythagoreanTriplet.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 )
+var globalResult []int
 
 func sumSlice(input []int) int {
 	// sum the numbers in a given int slice
@@ -14,14 +15,32 @@ func sumSlice(input []int) int {
 	return total
 }
 
+func productSlice(input []int) int {
+	// find the product of the values in a slice
+	total := 1
+	for _, value := range input {
+		total = total * value
+	}
+	return total
+}
+
 func generatePermutations(numbers []int, target int, partial []int) {
 	// generate all 3-number permutations of the numbers list.
 	// if the sum of a 3-number permutation == the target, 
 	// return the 3-number permutation.
 	s := sumSlice(partial)
-	if s == target {
-		fmt.Println("Target found, numbers are:")
-		fmt.Println(partial)
+
+	if s == target { // check to see if we've found our triplet
+		if len(partial) == 3 {
+			a := partial[0] 
+			b := partial[1]
+			c := partial[2]
+			if ( a < b && b < c) {
+				if (a*a + b*b == c*c) {
+					globalResult = partial // we've found it
+				}
+			}
+		}
 	}
 
 	for i := 0; i < len(numbers); i++ {
@@ -29,7 +48,8 @@ func generatePermutations(numbers []int, target int, partial []int) {
 		remaining := numbers[i+1:] // Golang slicing, i+1 to the end
 
 		// This is a horrible hack, necessary because of how slices
-		// work in Go. Explanation at the bottom of this file.
+		// work in Go- append keeps a pointer to the same slice, so I needed
+		// to copy the slice here to get a truly new one. 
 
 		// make a new array of a size one larger than the current size
 		// of "partial"
@@ -47,28 +67,45 @@ func findTriplet(tripletSum int) ([]int) {
 	// given a number, find the pythagorean triplet such that its 
 	// sum equals the given number. note that a < b < c.
 	var partial []int
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	// result := generatePermutations(numbers, tripletSum, partial)
+	// make a slice and fill it, using tripletSum arbitrarily
+	numbers := make([]int, tripletSum)
+	for i := 0; i < len(numbers); i++ {
+		numbers[i] = i
+	}
 	generatePermutations(numbers, tripletSum, partial)
-	return nil
+	return globalResult // euuuugh
 }
 
 func test() {
-	result := findTriplet(12)
-	expectedResult := []int{3, 4, 5}
+	resultSlice := findTriplet(12)
+	expectedResultSlice := []int{3, 4, 5}
 
-	for i := 0; i < len(expectedResult); i++ {
-		if result[i] != expectedResult[i] {
+	for i := 0; i < len(expectedResultSlice); i++ {
+		if resultSlice[i] != expectedResultSlice[i] {
 			fmt.Fprintf(os.Stderr, 
 				"***Test failed*** \n Wanted: %i, Received: %i \n", 
-				expectedResult, result)
+				expectedResultSlice, resultSlice)
 		    os.Exit(1)
 		}
 	}
+
+	resultProduct := productSlice(resultSlice)
+	expectedResultProduct := 60
+	if resultProduct != expectedResultProduct {
+		fmt.Fprintf(os.Stderr, 
+			"***Test failed*** \n Wanted: %i, Received: %i \n", 
+			expectedResultProduct, resultProduct)
+	    os.Exit(1)
+	}
+
+	globalResult = nil // reset global
 	fmt.Println("***Test Passed***")
 }
 
 func main() {
 	// test()
-	findTriplet(12)
+	sliceResult := findTriplet(1000)
+	fmt.Println(sliceResult)
+	product := productSlice(sliceResult)
+	fmt.Println(product)
 }
