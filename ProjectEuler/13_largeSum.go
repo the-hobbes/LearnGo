bcf21ad3517226519d6ca49b662e4ef952c96a07
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-07-14 09:02:34 -0700

    Finished string to int array converter.

--- ProjectEuler/13_largeSum.go
+++ ProjectEuler/13_largeSum.go
@@ -7,7 +7,9 @@ import (
 	"bufio"
 	"os"
 	"log"
+	"strings"
 	"strconv"
+	"sort"
 )
 
 var INPUT string // input global var
@@ -21,6 +23,14 @@ func check(e error) {
 func castToIntArray(target string) []int {
 	// used to convert a string of integers to an array of actual integers
 	// i, _ := strconv.Atoi(scanner.Text())
+	splitString := strings.Split(target, "")
+	arr := make([]int, len(splitString))
+	for i := 0; i < len(splitString); i++ {
+		s, err := strconv.Atoi(splitString[i])
+		check(err)
+		arr[i] = s
+	}
+	return arr
 }
 
 func largeSum() {
@@ -28,14 +38,23 @@ func largeSum() {
 	file, err := os.Open("largeSumInput")
     check(err)
     defer file.Close()
-    
+    totalSum := 0
+
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
 	    // fmt.Println(scanner.Text())
-	    // cast string to int
+	    // cast string to int array
 	    arr := castToIntArray(scanner.Text())
-	    // sort the arr
+	    // sort the array
+	    sort.Sort(sort.Reverse(sort.IntSlice(arr)))
+	    fmt.Println(arr)
 	    // sum the 11 most significant digits (easy w/sorting)
+	    sum := 0
+	    for i := 0; i < 11; i++ {
+	    	sum = sum + arr[i]
+	    }
+	    totalSum = totalSum + sum
+	    fmt.Println(totalSum)
 	    // http://math.stackexchange.com/questions/184397/10-most-significant-digits-of-the-sum-of-a-100-50-digit-numbers
 	}
 
