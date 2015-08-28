83f8f0a4476417b0f310aebd8c6a4161ecdc0446
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-08-28 10:34:36 -0700

    string building example

--- GoTricks.go
+++ GoTricks.go
@@ -3,3 +3,14 @@ buf := make([]byte, 1<<16)
 runtime.Stack(buf, true)
 fmt.Printf("%s", buf)
 
+// In general, when you want to build up a string over a series of statements, 
+// it's much more efficient to use a bytes.Buffer than to repeatedly concatenate:
+
+func catFoos(foos []string) string {
+  var b bytes.Buffer
+  for _, foo := range foos {
+    b.WriteString(foo)
+  }
+  return b.String()
+}
+
